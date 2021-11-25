c932dd7332990e5da2a2f6de9b774011cbec2f89
Ankit <akpareek4@gmail.com>
2021-11-25 19:27:04 +0530

    Now this Program works as expected.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,27 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        // write your code here
+
         int firstPatternNumber = 1;
         int secondPatternNumber = 0;
 
-        for (int lineNumber=1; lineNumber<=4;lineNumber++) {                                                   //outer loop for number of line
-            for(int blank=1; blank<lineNumber ; blank++){                                    //loop for blanks
+//outer loop for number of lines
+
+        for (int lineNumber=1; lineNumber<=4;lineNumber++) {
+
+//loop for blanks
+
+            for(int blank=1; blank<lineNumber ; blank++){
                 System.out.print(" ");
             }
-            for(int x=1;x<6-lineNumber;x++){                                                 //Inner loop for printing 1st Pattern
+
+//Inner loop for printing 1st Pattern
+
+            for(int x=1;x<6-lineNumber;x++){
                 System.out.print(firstPatternNumber++ + "*");
             }
+
+//Switch cases to change Second pattern's Starting Number
             switch (lineNumber) {
                 case 1 -> secondPatternNumber = 17;
                 case 2 -> secondPatternNumber = 14;
@@ -21,7 +31,11 @@ public class Main {
                 case 4 -> secondPatternNumber = 11;
             }
 
-            for(int y=1; y<6-lineNumber ; y++){                                              //Inner loop for printing 2nd Pattern
+//Inner loop for printing 2nd Pattern
+
+            for(int y=1; y<6-lineNumber ; y++){
+
+//Condition to avoid two stars in every line.
                 if(y == 1){
                     System.out.print(secondPatternNumber++);
                 }
